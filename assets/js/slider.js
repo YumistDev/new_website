7dbee5b121b9cd9f9ac35b00d47ee92054a69f9a
SlashG <slashg@yumist.com>
2015-08-31 15:49:14 +0530

    [FEATURE]
    - Font files added.
    - Font.css in 'assets/css' adds fonts to the webpage
    - All screenshots added (optimally compressed)
    
    --
    Author : SlashG

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,9 +1,13 @@
 var i = 0; var path = new Array();
 // LIST OF IMAGES
-path[0] = "assets/images/1.png";
-path[1] = "assets/images/3.png";
-path[2] = "assets/images/4.png";
-path[3] = "assets/images/5.png";
+path[0] = "assets/images/0.jpg";
+path[1] = "assets/images/1.jpg";
+path[2] = "assets/images/2.jpg";
+path[3] = "assets/images/3.jpg";
+path[4] = "assets/images/4.jpg";
+path[5] = "assets/images/5.jpg";
+path[6] = "assets/images/6.jpg";
+path[7] = "assets/images/7.jpg";
 
 function swapImage()
 {
