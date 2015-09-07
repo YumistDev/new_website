a0c90eb33dd481b4eb8f89197d24318f327b897c
SlashG <slashg@yumist.com>
2015-09-07 12:44:32 +0530

    [FEATURE + REFACTORING]
    - footer style reflected in Maps.html
    - 'HOME' nav item removed from index.html
    - Yumist logo added to source code
    - Google Analytics code added
    - Slide animation added to slideshow.
    
    --
    
    Author : SlashG

--- website/assets/js/slider.js
+++ website/assets/js/slider.js
@@ -9,11 +9,25 @@ path[5] = "website/assets/images/5.jpg";
 path[6] = "website/assets/images/6.jpg";
 path[7] = "website/assets/images/7.jpg";
 
+var slideInRightAnimationClass = "animate-slide-in-right";
+
 function swapImage()
 {
-  document.slide.src = path[i];
-  if(i < path.length - 1) i++;
-  else i = 0;
-  setTimeout("swapImage()",3000);
+
+	document.previousImage.src = path[i];
+	//var image = document.getElementById('screenshot-holder');
+   	if(i < path.length - 1) 
+  	{
+  		i++;
+  	}
+  	else 
+	{
+		i = 0;
+	}
+	document.slide.src = path[i];
+	document.slide.classList.remove(slideInRightAnimationClass);
+	document.slide.offsetWidth = document.slide.offsetWidth;
+	document.slide.classList.add(slideInRightAnimationClass);
+  	setTimeout("swapImage()",3000);
 }
 window.onload=swapImage;
